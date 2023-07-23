d4aeda89a09fc5a1cc0aefebdfece63c1a5ee852
Vinh Lam <vlam@opswat.com>
2023-07-23 00:13:18 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const { readFileSync, existsSync} = require('fs');
+const core = require('@actions/core')
+const { downloadJar, runScan} = require('./pipeline-scan')
+
+// get input params
+var parameters = {}
+
+const who_to_greet = core.getInput('who_to_greet', {required: true} );
+parameters['who_to_greet'] = who_to_greet
+
+async function run (parameters){
+    downloadJar()
+
+    core.info('Running the Pipeline Scan')
+    let scanCommandOutput = await runScan('java -jar opswat-0.0.1-SNAPSHOT.jar ' + parameters['who_to_greet'])
+
+    core.info('Pipeline Scan Output')
+    core.info(scanCommandOutput)
+    core.setFailed(scanCommandOutput)
+	core.setOutput("time", "1984");
+}
+
+run(parameters)
---
+++ src/pipeline-scan.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const { exec, execSync, spawn } = require('child_process');
+const core = require('@actions/core');
+
+exports.downloadJar = function () { 
+    core.info('Downloading scan.jar')
+    try {
+        var downloadJar = `curl -sSO https://metascansuppliers.s3.us-west-2.amazonaws.com/opswat-0.0.1-SNAPSHOT.jar`;
+        var getDownloadOutput = execSync(downloadJar).toString()
+        core.info('scan.jar downloaded')
+        
+    }
+    catch(error){
+        core.info(`Status Code: ${error.status} with '${error.message}'`);
+        
+    }
+};
+
+
+exports.runScan = function(scanCommand){  
+    let commandOutput = ''
+    try {
+        execSync(scanCommand)
+    } catch (ex){
+        commandOutput = ex.stdout.toString()
+    }
+    return commandOutput
+}
