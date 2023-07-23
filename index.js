55bbf09052e9eb2579cf7a2acb1cd814417a2f78
Vinh Lam <vlam@opswat.com>
2023-07-23 01:11:28 -0400

    update parameter

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const { downloadJar, runScan} = require('./pipeline-scan')
 // get input params
 var parameters = {}
 
-const who_to_greet = core.getInput('who_to_greet', {required: true} );
-parameters['who_to_greet'] = who_to_greet
+const who_to_greet = core.getInput('who-to-greet', {required: true} );
+parameters['who-to-greet'] = who_to_greet
 
 async function run (parameters){
     downloadJar()
 
     core.info('Running the Pipeline Scan')
-    let scanCommandOutput = await runScan('java -jar opswat-0.0.1-SNAPSHOT.jar ' + parameters['who_to_greet'])
+    let scanCommandOutput = await runScan('java -jar opswat-0.0.1-SNAPSHOT.jar ' + parameters['who-to-greet'])
 
     core.info('Pipeline Scan Output')
     core.info(scanCommandOutput)
