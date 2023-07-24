89b3192b03b41c30d2eb16678afa230fa294b1ff
Vinh Lam <vlam@opswat.com>
2023-07-24 18:42:51 -0400

    add jar file, update code

--- index.js
+++ index.js
@@ -5,14 +5,24 @@ const { downloadJar, runScan} = require('./pipeline-scan')
 // get input params
 var parameters = {}
 
-const who_to_greet = core.getInput('who-to-greet', {required: true} );
-parameters['who-to-greet'] = who_to_greet
+const scanURL = core.getInput('scan-url', {required: true} );
+parameters['scan-url'] = scanURL
+
+const apikey = core.getInput('apikey', {required: false} );
+parameters['apikey'] = apikey
+
+const folder = core.getInput('folder', {required: true} );
+parameters['folder'] = folder
+
+const logfile = core.getInput('logfile', {required: true} );
+parameters['logfile'] = logfile
+
 
 async function run (parameters){
-    downloadJar()
+    //downloadJar()
 
     core.info('Running the Pipeline Scan')
-    let scanCommandOutput = await runScan('java -jar opswat-0.0.1-SNAPSHOT.jar ' + parameters['who-to-greet'])
+    let scanCommandOutput = await runScan('java -jar scanner.jar -url ' + scanURL + ' -f ' + folder + ' -l ' + logfile + ' -k ' + apikey)
 
     core.info('Pipeline Scan Output')
     core.info(scanCommandOutput)
